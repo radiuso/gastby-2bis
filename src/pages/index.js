066240fd6d5bc9b34d4882fe3d65cd4b987e0bc2
radiusario <radiusario@gmail.com>
2018-04-04 19:55:20 +0200

    Add bulimia and sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
+  <div className="container">
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/" className="button is-text">Go to page 2</Link>
   </div>
 )
 
