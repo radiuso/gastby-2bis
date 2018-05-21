11fb96614cb747bb5dc2612b9bcc518910b2e69d
radiusario <radiusario@gmail.com>
2018-05-21 19:35:03 +0200

    above the fold v2

--- src/components/Atf/Atf.js
+++ src/components/Atf/Atf.js
@@ -6,16 +6,16 @@ import './Atf.scss';
 
 const Atf = () => (
   <div className="atf">
-    <img src="static/images/logo.svg" className="atf--logo" />
+    <img src="images/logo.svg" className="atf--logo" />
 
     <h2 className="atf--subtitle">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis risus dolor, eleifend a enim at, feugiat lobortis magna.
     </h2>
-    <img src="static/images/rect.svg" className="atf--rect" />
+    <img src="images/rect.svg" className="atf--rect" />
 
-    <img src="static/images/atf_terrasse.png"
-        srcSet="static/images/atf_terrasse@2x.png 2x,
-                static/images/atf_terrasse@3x.png 3x"
+    <img src="images/atf_terrasse.png"
+        srcSet="images/atf_terrasse@2x.png 2x,
+                images/atf_terrasse@3x.png 3x"
         className="atf--image" />
   </div>
 );
